b1c780b25d2c440bab6433c937178412697abc11
PedroRio1357 <103968662+PedroRio1357@users.noreply.github.com>
2022-04-19 15:35:02 -0300

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,40 +1,45 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Menu {
-	
+
+	public static Scanner adm = new Scanner(System.in);
+
 	public static String principal() {
 		System.out.println("Escolha uma opção do menu inicial");
-		System.out.println("1) Login");
-		System.out.println("2) Se cadastrar como cliente");
+		System.out.println("1) Login.");
+		System.out.println("2) Se cadastrar.");
+		System.out.println("3) Ver lista de itens.");
+		System.out.println("4) Ver lista de itens ordenada (Categorizada).");
 		System.out.println("S) Sair");
-		String opcao = Locadora.leitor.nextLine(); 
+		String opcao = adm.nextLine();
 		return opcao;
 	}
-	
-	public static Usuario login(ArrayList<Usuario> usuarios) {
-		System.out.println("Informe seus dados de usuário já cadastrados");
+
+	public static Cliente login(ArrayList<Cliente> cliente) {
+		System.out.println("Informe seus dados de usuário já cadastrado");
 		System.out.print("Email:");
-		String email = Locadora.leitor.nextLine();
-		System.out.print("Senha:");		
-		String senha = Locadora.leitor.nextLine();
-		for (Usuario usuario : usuarios) {
-			if (usuario.getEmail().equals(email) &&
-					usuario.getSenha().equals(senha)) {
+		String email = adm.nextLine();
+		System.out.print("Senha:");
+		String senha = adm.nextLine();
+		for (Cliente usuario : cliente) {
+			if (usuario.getEmail().equals(email) && usuario.getSenha().equals(senha)) {
 				return usuario;
 			}
 		}
 		return null;
 	}
 
-	public static String gerente() {
-		System.out.println("Bem vindo gerente");
+	public static String admin() {
+		System.out.println("Bem vindo Admin!");
 		System.out.println("Escolha uma opção do menu");
-		System.out.println("1) Cadastrar um novo cliente");
+		System.out.println("1) Cadastrar um novo item");
 		System.out.println("2) Listar clientes");
+		System.out.println("3) Mostrar Itens.");
 		System.out.println("v) Voltar para o menu incial");
-		String opcao = Locadora.leitor.nextLine();
+		String opcao = adm.nextLine();
 		return opcao;
 	}
-	
-
 }
+
+
